e65f0844a10906b240d025cd58f903f22b6c34c5
JeodC <47716344+JeodC@users.noreply.github.com>
2024-01-22 10:44:24 -0500

    Update install script

--- F-Zero Pocket.sh
+++ F-Zero Pocket.sh
@@ -33,9 +33,32 @@ cd $GAMEDIR
 
 # Run the installer file if it hasn't been run yet
 if [ ! -f "$GAMEDIR/installed" ]; then
-  chmod +xw install.sh # Ensure the script is executable
-  echo "Unpacking the game...please wait (might take a while!)" > /dev/tty0
-  ./install.sh 2>&1 | tee $GAMEDIR/install.txt
+	EXE="./gamedata/FZero_Pocket.exe"
+	TRACKS="gamedata/Tracks"
+	ASSETS="assets"
+	DATA="gamedata"
+
+	# Extract .trk files to the Tracks folder
+	echo "Extracting tracks into $TRACKS..." > /dev/tty0
+	./lib/7za e "$EXE" "*.trk" -o"$TRACKS"
+
+	# Extract .ogg files to the Assets folder
+	echo "Extracting music into $ASSETS..." > /dev/tty0
+	./lib/7za e "$EXE" "*.ogg" -o"$ASSETS"
+
+	# Extract .win and .ini files to the Data folder
+	echo "Extracting data into $DATA..." > /dev/tty0
+	./lib/7za e -y "$EXE" "*.win" "*.ini" -o"$DATA"
+
+	# Rename data.win
+	mv "$DATA/data.win" "$DATA/game.droid"
+
+	# Create a new zip file game.apk from specified directories
+	echo "Zipping $ASSETS into apk..." > /dev/tty0
+	./lib/7za a -r "./game.apk" "./$ASSETS"
+
+	# Delete the executable file after extraction
+	rm "$EXE"
 fi
 
 # Make sure uinput is accessible so we can make use of the gptokeyb controls
