590d0f2e9607135ebcbc958bd74478e515e451ed
JeodC <47716344+JeodC@users.noreply.github.com>
2024-01-22 10:55:17 -0500

    Update F-Zero Pocket.sh

--- F-Zero Pocket.sh
+++ F-Zero Pocket.sh
@@ -32,33 +32,43 @@ export GMLOADER_SAVEDIR="$GAMEDIR/gamedata/"
 cd $GAMEDIR
 
 # Run the installer file if it hasn't been run yet
+set -e  # Exit on error
+set -x  # Enable debugging
 if [ ! -f "$GAMEDIR/installed" ]; then
 	EXE="./gamedata/FZero_Pocket.exe"
 	TRACKS="gamedata/Tracks"
 	ASSETS="assets"
 	DATA="gamedata"
+	
+	# Redirect output to install.log only for the commands within the if condition
+	(
+		exec > >(tee -a "$GAMEDIR/install.log") 2>&1
 
-	# Extract .trk files to the Tracks folder
-	echo "Extracting tracks into $TRACKS..." > /dev/tty0
-	./lib/7za e "$EXE" "*.trk" -o"$TRACKS"
+		# Extract .trk files to the Tracks folder
+		echo "Extracting tracks into $TRACKS..." > /dev/tty0
+		./lib/7za e "$EXE" "*.trk" -o"$TRACKS"
 
-	# Extract .ogg files to the Assets folder
-	echo "Extracting music into $ASSETS..." > /dev/tty0
-	./lib/7za e "$EXE" "*.ogg" -o"$ASSETS"
+		# Extract .ogg files to the Assets folder
+		echo "Extracting music into $ASSETS..." > /dev/tty0
+		./lib/7za e "$EXE" "*.ogg" -o"$ASSETS"
 
-	# Extract .win and .ini files to the Data folder
-	echo "Extracting data into $DATA..." > /dev/tty0
-	./lib/7za e -y "$EXE" "*.win" "*.ini" -o"$DATA"
+		# Extract .win and .ini files to the Data folder
+		echo "Extracting data into $DATA..." > /dev/tty0
+		./lib/7za e -y "$EXE" "*.win" "*.ini" -o"$DATA"
 
-	# Rename data.win
-	mv "$DATA/data.win" "$DATA/game.droid"
+		# Rename data.win
+		mv "$DATA/data.win" "$DATA/game.droid"
 
-	# Create a new zip file game.apk from specified directories
-	echo "Zipping $ASSETS into apk..." > /dev/tty0
-	./lib/7za a -r "./game.apk" "./$ASSETS"
+		# Create a new zip file game.apk from specified directories
+		echo "Zipping $ASSETS into apk..." > /dev/tty0
+		./lib/7za a -r "./game.apk" "./$ASSETS"
 
-	# Delete the executable file after extraction
-	rm "$EXE"
+		# Delete the executable file after extraction
+		rm "$EXE"
+	
+		# Create 'installed' file to indicate successful installation
+		touch "$GAMEDIR/installed"
+	)
 fi
 
 # Make sure uinput is accessible so we can make use of the gptokeyb controls
